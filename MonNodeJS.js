557e5fc7f7ef256ef7017cbc5e870e6195be182b
LukasAppleFan <41242166+LukasAppleFan@users.noreply.github.com>
2020-04-14 14:44:15 +0200

    Nouveau

--- MonNodeJS.js
+++ MonNodeJS.js
@@ -1,3 +1,18 @@
-let A;
-A = 42;
+let A = (42 + "0") / (4**2);
 console.log(A);
+
+let num1 = 0;
+{
+	num1 = 1;
+	const num2 = 0;
+}
+console.log(num1);
+//console.log(num2);
+
+console.log("J'ai " + A + "€")
+console.log(`J'ai ${A} €`);
+
+const x = 3;
+const y = 7;
+console.log(`${x} + ${y} = ${x + y}`);
+console.log(x + " + " + y + " = " + (x + y));
